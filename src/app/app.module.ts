508eaa31ca49820e8651550c247bf1f036053a6b
szary7846 <danielszarynski@interia.pl>
2019-11-20 15:04:26 +0100

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { AppComponent } from './app.component';
+import { XdddComponent } from './xddd/xddd.component';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, HelloComponent, XdddComponent ],
+  bootstrap:    [ HelloComponent ]
 })
 export class AppModule { }
